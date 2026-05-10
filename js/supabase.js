23d7ec75e2b90b53aabc06af21fd0abe83bd783a
Ukima28 <stepanov.oleg2019@gmail.com>
2026-05-10 07:45:54 +0300

    Add files via upload

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+
+const SUPABASE_URL = 'https://oeybefxeelxduzbuxobe.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9leWJlZnhlZWx4ZHV6YnV4b2JlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzNjMwMTksImV4cCI6MjA5MzkzOTAxOX0.PW30591ZJlHsVtSzHmEQ8XrEoHP9mEFMyoGAsTSlQu4';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
