9216f70a2f421297671f1a4efd182e2ac4623e67
Ukima28 <stepanov.oleg2019@gmail.com>
2026-05-15 07:03:40 +0300

    Update shell.js

--- js/shell.js
+++ js/shell.js
@@ -49,12 +49,12 @@ export async function renderShell(activePage) {
         </button>
         <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
           <a href="${BASE}/pages/cases.html" class="text-decoration-none">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110 30" width="110" height="30" role="img">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 90 30" width="90" height="30" role="img">
               <title>MORPHEOS PATHOLOGY</title>
-              <text font-family="Arial,sans-serif" font-size="13" font-weight="700">
+              <text font-family="Arial,sans-serif" font-size="13" font-weight="700" textLength="90" lengthAdjust="spacingAndGlyphs">
                 <tspan x="0" y="14" fill="#1a1a1a">MORPHE</tspan><tspan fill="#BA1A32">OS</tspan>
               </text>
-              <text x="0" y="27" font-family="Arial,sans-serif" font-size="9" font-weight="400" letter-spacing="1.5" fill="#94a3b8">PATHOLOGY</text>
+              <text x="0" y="27" font-family="Arial,sans-serif" font-size="9" font-weight="400" fill="#94a3b8" textLength="90" lengthAdjust="spacingAndGlyphs">PATHOLOGY</text>
             </svg>
           </a>
         </h1>
